c88c3d6381b6d0bed281aab016453e95f1b005c0
Tom Flídr <tomflidr@gmail.com>
2022-10-21 17:39:17 +0200

    release v5.0.2

--- src/MvcCore/Ext/Tools/Collections/Map.php
+++ src/MvcCore/Ext/Tools/Collections/Map.php
@@ -21,7 +21,7 @@ implements		\Iterator, \ArrayAccess, \Countable, \JsonSerializable {
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.0.1';
+	const VERSION = '5.0.2';
 
 	/*********************************************************************
 	 *                                                                   *
